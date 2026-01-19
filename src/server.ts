0355bf0bbeac1a86c303f95919e889a0e9ad08f3
Koipix <koi.devt@gmail.com>
2026-01-19 23:05:28 +0800

    minor: post request

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,26 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+//get requests
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running..");
 });
 
 app.get("/test", (req: Request, res: Response) => {
+  console.log("get request recieved, sending response..")
+
   res.json({
     message: "Request works.."
+  });
+});
+
+//post requests
+app.post("/upload", (req: Request, res: Response) => {
+  console.log("post request recieved, sending response..")
+
+  res.json({
+    success: true,
+    message: "Post request received"
   });
 });
 
